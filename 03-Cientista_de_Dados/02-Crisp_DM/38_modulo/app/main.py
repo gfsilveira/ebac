96beeace6fb1b8fc364fadeda132377713066f42
Guilherme Silveira <gfsilveira@gmail.com>
2025-01-14 18:44:50 -0300

    Crisp-DM, módulo 38, tarefa 02.3

--- 03-Cientista_de_Dados/02-Crisp_DM/38_modulo/app/main.py
+++ 03-Cientista_de_Dados/02-Crisp_DM/38_modulo/app/main.py
@@ -3,6 +3,11 @@ import streamlit as st
 
 def inicia() -> None:
     st.markdown("Entrada")
+    uploaded_files = st.file_uploader("Choose a CSV file")
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        st.write("filename:", uploaded_file.name)
+        st.write(bytes_data)
 
 
 if __name__ == "__main__":
