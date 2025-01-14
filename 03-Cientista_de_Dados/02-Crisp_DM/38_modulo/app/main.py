53a452fa73c3c9c0ffee1a79302bb0a0c8752c57
Guilherme Silveira <gfsilveira@gmail.com>
2025-01-14 18:55:35 -0300

    Crisp-DM, módulo 38, tarefa 02.5

--- 03-Cientista_de_Dados/02-Crisp_DM/38_modulo/app/main.py
+++ 03-Cientista_de_Dados/02-Crisp_DM/38_modulo/app/main.py
@@ -2,11 +2,16 @@ import streamlit as st
 import pandas as pd
 
 def inicia() -> None:
+    # Entrada
     st.markdown("Entrada")
-    uploaded_file = st.file_uploader("Choose a CSV file")
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        st.dataframe(df.head())
+
+    with st.sidebar:
+        # Upload de arquivo
+        uploaded_file = st.file_uploader("Choose a CSV file")
+        if uploaded_file is not None:
+            df = pd.read_csv(uploaded_file)
+            st.dataframe(df.head())
 
 if __name__ == "__main__":
+    st.set_page_config(layout="wide")
     inicia()
